3958e58f31c396fd45bc965f4f397d61b3b43464
lamgak12 <tntnqk12@naver.com>
2025-02-09 23:42:29 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/example/scheduledevelop/ScheduleDevelopApplication.java
@@ -0,0 +1,21 @@
+package com.example.scheduledevelop;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class ScheduleDevelopApplication {
+
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+        SpringApplication.run(ScheduleDevelopApplication.class, args);
+    }
+
+}
